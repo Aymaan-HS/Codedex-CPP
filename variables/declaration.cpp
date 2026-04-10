62ed774e55b3ac67c1a938a196c2afedfdbe25b0
Aymaan Haider Saiyad <aymaanhaider2108@gmail.com>
2026-04-10 10:50:03 +0530

    Update declaration.cpp

--- variables/declaration.cpp
+++ variables/declaration.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+int main() {
+  // Write code here 💖
+  std::string name = "Lady Bird";
+  int grade = 12;
+  double gpa = 12;
+  bool honor = false;
+  int year = 2024;
+  int age = 28;
+  double pi = 3.14;
+  double meal_cost = 12.99;
+  char mention = '@';
+  char hashtag = '#';
+  std::string message = "good night";
+  std::string username = "@potus";
+}
