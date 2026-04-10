dba38b49d78254ee16c1b8e08ec4709a176e17c5
Aymaan Haider Saiyad <aymaanhaider2108@gmail.com>
2026-04-10 10:47:46 +0530

    Create goodbye.cpp

---
+++ hello-world/goodbye.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Alright mate gtg, see ya later!\n";
+  std::cout << "Alr gng cya laterr..\n";
+  std::cout << "Yo bro, I gotta pull up to my house, gon call ya laterr.\n";
+  std::cout << "Goodbye love <3\n";
+  std::cout << "And it's time to go home, call me once you reach\n";
+}
