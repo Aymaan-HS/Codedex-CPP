82bc3d003a466df7e754bb7ac39fa8f4aa61f9ed
Aymaan Haider Saiyad <aymaanhaider2108@gmail.com>
2026-04-10 10:51:24 +0530

    Add Reddit IPO values to ipo.cpp
    Initialize Reddit IPO values and print the final value.

---
+++ variables/ipo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  // Write code here 💖
+  // Reddit IPO
+  double reddit = 34;
+reddit = 48.95;
+reddit = 44.87;
+reddit = 57.55;
+reddit = 64.20;
+reddit = 59.24;
+reddit = 61.92;
+  
+  std::cout << reddit;
+}
