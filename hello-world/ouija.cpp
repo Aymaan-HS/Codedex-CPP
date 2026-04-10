6a312737be93a87a5b2b70580c60c04c29048c68
Aymaan Haider Saiyad <aymaanhaider2108@gmail.com>
2026-04-10 10:48:25 +0530

    Create ouija.cpp with Ouija board simulation
    Added a simple Ouija board simulation in C++ that displays a description and layout.

---
+++ hello-world/ouija.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+  std::cout << "Ouija Board is a flat wood marked with letters of the alphabet, the numbers 0-9, and the words yes and no.\n";
+  std::cout << " It was believed that the dead can contact the living through it. 👻\n";
+  std::cout << "----------------------------------\n";
+  std::cout << "|          x YES   NO x          |\n";
+  std::cout << "|                                |\n";
+  std::cout << "|         ABCDEFGHIJKLM          |\n";
+  std::cout << "|         NOPQRSTUVWXYZ          |\n";
+  std::cout << "|                                |\n";
+  std::cout << "|           1234567890           |\n";
+  std::cout << "----------------------------------\n";
+  }
