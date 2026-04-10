73b88d6d4eb44cac84a32d9bc06a7250b7772185
Aymaan Haider Saiyad <aymaanhaider2108@gmail.com>
2026-04-10 10:52:10 +0530

    Update ipo.cpp

--- variables/ipo.cpp
+++ variables/ipo.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 
 int main() {
-  // Write code here 💖
-  // Reddit IPO
-  double reddit = 34;
-reddit = 48.95;
-reddit = 44.87;
-reddit = 57.55;
-reddit = 64.20;
-reddit = 59.24;
-reddit = 61.92;
+double reddit = 34;
+  reddit = 48.95;
+  reddit = 44.87;
+  reddit = 57.55;
+  reddit = 64.20;
+  reddit = 59.24;
+  reddit = 61.92;
   
-  std::cout << reddit;
+std::cout << reddit;
 }
