8a471f40c1dab9cbaebde0d0b7a3478f33265f78
Aymaan Haider Saiyad <aymaanhaider2108@gmail.com>
2026-04-10 10:51:43 +0530

    Create temperature.cpp

---
+++ variables/temperature.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  double fahrenheit = 40;
+  double celsius;
+  
+  celsius=(fahrenheit-32)/1.8;
+
+  std::cout << "Temperature in Brooklyn, NY is " << celsius << " °C.\n";
+}
